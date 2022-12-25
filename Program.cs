e7157029b8fb0d1fd56605d27453222f42d960f2
youmnaelgzery <120684747+youmnaelgzery@users.noreply.github.com>
2022-12-25 20:06:19 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,67 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp18
+namespace perfect
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Declare the variables
-            int x, y, i, j, flag;
+            int n, i, sum;
+            int mn, mx;
 
-            // Ask user to enter lower value of interval
-            Console.WriteLine("Enter lower bound of the interval:");
+            Console.Write("\n\n");
+            Console.Write("Find perfect numbers within a given number of range:\n");
+            Console.Write("------------------------------------------------------");
+            Console.Write("\n\n");
 
-
-
-            // Take input
-            x = int.Parse(Console.ReadLine());
-
-            // Ask user to enter upper value of interval
-            Console.WriteLine("\nEnter upper bound of the interval:");
-
-
-            // Take input
-            y = int.Parse(Console.ReadLine());
-
-            // Print display message
-            Console.WriteLine("\nPrime numbers between " +
-                              "{0} and {1} are: ", x, y);
-
-            // Traverse each number in the interval
-            // with the help of for loop
-            for (i = x; i <= y; i++)
+            Console.Write("Input the starting range or number : ");
+            mn = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the ending range of number : ");
+            mx = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The Perfect numbers within the given range : ");
+            for (n = mn; n <= mx; n++)
             {
-
-                // Skip 0 and 1 as they are
-                // neither prime nor composite
-                if (i == 1 || i == 0)
-                    continue;
-
-                // flag variable to tell
-                // if i is prime or not
-                flag = 1;
-
-                for (j = 2; j <= i / 2; ++j)
+                i = 1;
+                sum = 0;
+                while (i < n)
                 {
-                    if (i % j == 0)
-                    {
-                        flag = 0;
-                        break;
-                    }
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
                 }
-
-                // flag = 1 means i is prime
-                // and flag = 0 means i is not prime
-                if (flag == 1)
-                    Console.WriteLine(i);
+                if (sum == n)
+                    Console.Write("{0} ", n);
             }
+            Console.Write("\n");
             Console.ReadKey();
         }
-
     }
 }
-    
-
-       
